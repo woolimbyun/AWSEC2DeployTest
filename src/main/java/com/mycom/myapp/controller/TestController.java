979a1532f2625925a2b1e1847c6ab18d0c7d712a
woolimbyun <bwl9746@gmail.com>
2026-09-03 11:10:51 +0900

    first

--- src/main/java/com/mycom/myapp/controller/TestController.java
+++ src/main/java/com/mycom/myapp/controller/TestController.java
@@ -1,7 +1,8 @@
 package com.mycom.myapp.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
-
+import org.springframework.web.bind.annotation.RestController;
+@RestController
 public class TestController {
 
 	@GetMapping("/Hello")
